31d85d901e065b3007e9f6d65f3cfd9ba171c71c
Jorge Cano <jacinaustin@gmail.com>
2018-01-31 15:37:34 -0600

    loop

--- PythonTutorial.py
+++ PythonTutorial.py
@@ -5,3 +5,8 @@ distance = "1,599"
 print(a + " is your starting point.")
 print (b + " is your destination point")
 print ("The total distance from " + a + " to " + b + " is approxmiately " + distance + " miles.")
+
+for x in xrange(3):
+    print x
+else:
+    print 'Final x = %d' % (x)
