6acca944ea9acd08601f89741e0a3049b968dd62
Jorge Cano <jacinaustin@gmail.com>
2018-02-03 18:46:31 -0600

    different loops

--- PythonTutorial.py
+++ PythonTutorial.py
@@ -1,16 +1,22 @@
-a = "NYC"
-b = "Austin"
-distance = "1,599"
+# a = "NYC"
+# b = "Austin"
+# distance = "1,599"
+#
+# print(a + " is your starting point.")
+# print (b + " is your destination point")
+# print ("The total distance from " + a + " to " + b + " is approxmiately " + distance + " miles.")
+#
+# for x in xrange(3):
+#     print x
+# else:
+#     print 'Final x = %d' % (x)
 
-print(a + " is your starting point.")
-print (b + " is your destination point")
-print ("The total distance from " + a + " to " + b + " is approxmiately " + distance + " miles.")
+# for x in xrange(16):
+#     print x
+# else:
+#     print 'Final X = %d' % (x)
 
-for x in xrange(3):
-    print x
-else:
-    print 'Final x = %d' % (x)
 
-for x in xrange(16):
-    if x == 2:
-        break
+for x in xrange(1, 11):
+    for y in xrange(1, 11):
+        print '%d * %d = %d' % (x, y, x*y)
